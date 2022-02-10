cdddf95c7749126ef65a023ff4bb1f37c8cb6fcd
Jeff Morton <jmorton138@gmail.com>
2022-02-10 07:19:05 +0000

    Aligned add field buttons

--- cv-project/src/components/AddEduForm.js
+++ cv-project/src/components/AddEduForm.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class AddEduForm extends Component {
   render() {
     return (
-    <div>
+    <div className='button-container'>
         <button onClick={(e) => this.props.newEduForm(e)}>Add</button>
     </div>
     )
