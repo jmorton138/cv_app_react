c2f939a2382e8189691706d04bd2d6415ff94bfe
Jeff Morton <jmorton138@gmail.com>
2022-02-12 16:48:13 +0000

    Refactored Userinfo to functional component

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,29 +1,26 @@
 import React, { Component } from "react";
 
-class UserInfo extends Component {
+const UserInfo = props => {
 
-
-    render() {
         return (
             <div className="section-container">
                 <div className="field">
                     <label htmlFor="nameInput">Name</label>
-                    <input type="text" onChange={this.props.handleChange}  id="nameInput" name="name" value={this.props.info.name}></input>
+                    <input type="text" onChange={props.handleChange}  id="nameInput" name="name" value={props.info.name}></input>
                 </div>
 
                 <div className="field">
                     <label htmlFor="emailInput">Email</label>
-                    <input type="email" onChange={this.props.handleChange}  id="emailInput" name="email" value={this.props.info.email}></input>
+                    <input type="email" onChange={props.handleChange}  id="emailInput" name="email" value={props.info.email}></input>
                 </div>
  
                 <div className="field">
                     <label htmlFor="phoneInput">Phone</label>
-                    <input type="tel" onChange={this.props.handleChange} id="phoneInput" name="phone" value={this.props.info.phone}></input>
+                    <input type="tel" onChange={props.handleChange} id="phoneInput" name="phone" value={props.info.phone}></input>
                 </div>
 
             </div>
         );
-    }
 }
 
 export default UserInfo;
